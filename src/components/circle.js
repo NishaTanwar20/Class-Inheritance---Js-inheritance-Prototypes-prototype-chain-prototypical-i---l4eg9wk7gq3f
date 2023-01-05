25d777b499a1633651d2f29f1ec2ab204896ebad
Nisha Tanwar <103480687+NishaTanwar20@users.noreply.github.com>
2023-01-05 10:20:31 +0530

    updated

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,14 +1,28 @@
 // import using require
-const Shape = require('./shape.js');
-// declare class
-class Circle extends Shape{
-    constructor(color){
-        super(color);
-    }
-    calculateArea(){
+// const Shape = require('./shape.js');
+// // declare class
+// class Circle extends Shape{
+//     constructor(color){
+//         super(color);
+//     }
+//     calculateArea(){
 
-    }
+//     }
+// }
+
+// // export class using module.exports
+// module.exports=Circle;
+const Shape = require('./shape');
+
+class Circle extends Shape {
+  constructor(color, radius) {
+    super(color);
+    this.radius = radius;
+  }
+
+  calculateArea() {
+    return Math.PI * this.radius * this.radius;
+  }
 }
 
-// export class using module.exports
-module.exports=Circle;
+module.exports = Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,13 +1,29 @@
 // declare class
-class Shape{
-    drawShape(){
+// class Shape{
+//     drawShape(){
 
+//     }
+//     calculateArea(){
+
+//     }
+
+//     color='blue';
+// }
+// // export class using module.exports
+// module.exports=Shape;
+class Shape {
+    constructor(color) {
+      this.color = color;
     }
-    calculateArea(){
-
+  
+    drawShape() {
+      console.log(`Drawing a shape with color: ${this.color}`);
     }
-
-    color='blue';
-}
-// export class using module.exports
-module.exports=Shape;
+  
+    calculateArea() {
+      throw new Error('You must override the calculateArea method in a subclass');
+    }
+  }
+  
+  module.exports = Shape;
+  
